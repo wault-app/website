6c05571e1f2b0928b27520fc54a7be02df5c60a0
Gál Péter <pepyta118@gmail.com>
2021-06-10 15:36:35 +0200

    feat: copy password to clipboard

--- components/dashboard/accounts/AccountDialog/CopyPasswordButton.tsx
+++ components/dashboard/accounts/AccountDialog/CopyPasswordButton.tsx
@@ -1,13 +1,31 @@
 import { VpnKeyRounded as PasswordIcon } from "@material-ui/icons";
 import { ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import Clipboard from "@lib/client/clipboard";
+import { useSnackbar } from "notistack";
 
-const CopyPasswordButton = ({ password }: { password: string }) => (
-    <ListItem button>
-        <ListItemIcon>
-            <PasswordIcon />
-        </ListItemIcon>
-        <ListItemText>Copy password</ListItemText>
-    </ListItem>
-);
+const CopyPasswordButton = ({ password }: { password: string }) => {
+    const { enqueueSnackbar } = useSnackbar();
+
+    const copy = async () => {
+        try {
+            await Clipboard.copy(password);
+            enqueueSnackbar("Successfully copied password to clipboard");
+        } catch (e) {
+            console.error(e);
+            enqueueSnackbar("Failed to copy to clipboard", {
+                variant: "error",
+            });
+        };
+    };
+
+    return (
+        <ListItem button onClick={() => copy()}>
+            <ListItemIcon>
+                <PasswordIcon />
+            </ListItemIcon>
+            <ListItemText>Copy password</ListItemText>
+        </ListItem>
+    );
+}
 
 export default CopyPasswordButton;

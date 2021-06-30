bc9a7ec468c23e885c4ab47e1249f693331d3f4d
Péter Gál <pepyta118@gmail.com>
2021-06-30 10:38:47 +0200

    fix: lower generated encryption key size

--- lib/client/encryption/EncryptionKey.ts
+++ lib/client/encryption/EncryptionKey.ts
@@ -4,7 +4,7 @@ export default class EncryptionKey {
      * @param length how long should be the key
      * @returns a random string
      */
-    public static async generate(length = 1024) {
+    public static async generate(length = 128) {
         let resp = "";
         const pool = "0123456789abcdef";
 

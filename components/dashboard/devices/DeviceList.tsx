1794d14261e9fdcf014b22575af5f61a0094a9e8
Péter Gál <pepyta118@gmail.com>
2021-07-09 21:14:51 +0200

    feat: handle errors when failing to load devices

--- components/dashboard/devices/DeviceList.tsx
+++ components/dashboard/devices/DeviceList.tsx
@@ -1,5 +1,6 @@
 import Device, { DeviceType } from "@lib/client/api/Device";
-import { List } from "@material-ui/icons";
+import WrapperError from "@lib/server/error";
+import { Button, Grid, makeStyles, Typography } from "@material-ui/core";
 import { Fragment, useEffect, useState } from "react";
 import { AutoSizer, List as VirtualizedList } from "react-virtualized";
 import DeviceItem from "./DeviceItem";
@@ -7,20 +8,58 @@ import DeviceItem from "./DeviceItem";
 export type DeviceListProps = {};
 
 const DeviceList = (props: DeviceListProps) => {
+    const classes = useStyles();
+
     const [devices, setDevices] = useState<DeviceType[]>();
+    const [error, setError] = useState<WrapperError>();
 
     const load = async () => {
         try {
-            const { devices } = await Device.getAll()
+            setError(null);
+            const { devices } = await Device.getAll();
             setDevices(devices);
         } catch (e) {
-
+            setError(e);
         }
     };
 
     useEffect(() => {
         load();
     }, []);
+
+    if(error) {
+        return (
+            <Grid
+                container
+                spacing={2}
+                className={classes.root}
+            >
+                <Grid item xs={12}>
+                    <Typography variant={"h5"}>
+                        Failed to load your devices
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography>
+                        {error.message}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid container justify={"center"}>
+                        <Grid item>
+                            <Button
+                                onClick={() => load()}
+                                variant={"outlined"}
+                                color={"primary"}
+                            >
+                                Click here to retry
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        );
+    }
 
     if(!devices) {
         return (
@@ -58,5 +97,11 @@ const DeviceList = (props: DeviceListProps) => {
         </AutoSizer>
     );
 };
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        margin: theme.spacing(2)
+    },
+}));
 
 export default DeviceList;

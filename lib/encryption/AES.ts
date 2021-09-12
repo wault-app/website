59bfb74abefd228e9bcdbc3e6eec5074918f36b2
Gál Péter <pepyta118@gmail.com>
2021-09-12 19:08:45 +0200

    chore: remove unused rows inside AES module

--- lib/encryption/AES.ts
+++ lib/encryption/AES.ts
@@ -1,30 +1,14 @@
 import CryptoJS from "crypto-js";
 import CryptoJSAES from "crypto-js/aes";
 
-export default class AES {
-    private key: string;
-
-    constructor(secret: string) {
-        this.key = secret;
-    }
-
-    public encrypt(text: string) {
-        return CryptoJSAES.encrypt(text, this.key).toString();
-    }
-
-    public static encrypt(text: string, key: string) {
+const AES = {
+    encrypt: (text: string, key: string) => {
         return CryptoJSAES.encrypt(text, key).toString();
-    }
-
-    public decrypt(hash: string) {
-        if(!hash) return;
-
-        return CryptoJSAES.decrypt(hash, this.key).toString(CryptoJS.enc.Utf8);
-    }
-
-    public static decrypt(text: string, key: string) {
+    },
+    decrypt: (text: string, key: string) => {
         if(!text) return;
         return CryptoJSAES.decrypt(text, key).toString(CryptoJS.enc.Utf8);
-    }
+    },
+};
 
-}
+export default AES;

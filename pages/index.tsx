e7ba309aa53296d5eaae2011cbc392d25e28c8c2
Gál Péter <pepyta118@gmail.com>
2021-09-20 15:03:53 +0200

    style: use a grid-like overlay on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,19 @@ import { useKeycards } from "@components/KeycardProvider";
 import SafeItem from "@components/SafeItem";
 import NoSafeCreated from "@components/NoSafeCreated";
 import { Container, Grid } from "@mui/material";
+import { Box } from "@mui/system";
 
 const MainPage = () => {
     const { keycards } = useKeycards();
 
     return (
         <Fragment>
-            <Container maxWidth={"sm"} sx={{ pt: 2, pb: 2 }}>
+            <Box sx={{ p: 2 }}>
                 {keycards.length > 0 ? (
                     <Grid container spacing={2}>
                         {keycards.map(
                             (keycard) => (
-                                <Grid item xs={12} key={`safe-item-${keycard.safe.id}`} >
+                                <Grid item xs={12} md={6} lg={4} key={`safe-item-${keycard.safe.id}`} >
                                     <SafeItem
                                         keycard={keycard}
                                     />
@@ -27,7 +28,7 @@ const MainPage = () => {
                 ) : (
                     <NoSafeCreated />
                 )}
-            </Container>
+            </Box>
             <AddItemFAB />
         </Fragment>
     );

a88af25bbc0885409e76ed7cd493821ceac06a6c
Gál Péter <pepyta118@gmail.com>
2021-09-20 11:04:03 +0200

    perf: memoize theme inside tag

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -1,18 +1,24 @@
-import { Chip, ChipProps, createMuiTheme, ThemeProvider } from "@mui/material";
+import { Chip, ChipProps, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/system";
+import { useMemo } from "react";
 
 export type TagProps = ChipProps & {
     color?: any;
 };
 
 const Tag = (props: TagProps) => {
+    const theme = useMemo(
+        () => createTheme({
+            palette: {
+                primary: props.color,
+            }
+        })
+    , [props.color]);
+
     if(props.color) {
         return (
             <ThemeProvider
-                theme={createMuiTheme({
-                    palette: {
-                        primary: props.color,
-                    }
-                })}
+                theme={theme}
             >
                 <Chip
                     {...props}

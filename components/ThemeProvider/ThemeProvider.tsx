505242557ceeac2116fefa70025920476dc844e1
Gál Péter <pepyta118@gmail.com>
2021-10-06 14:31:25 +0200

    style: use browser color schema as default

--- components/ThemeProvider/ThemeProvider.tsx
+++ components/ThemeProvider/ThemeProvider.tsx
@@ -85,8 +85,10 @@ const DarkModeProvider = (props: DarkModeProviderProps) => {
     const [colorSchema, setColorSchema] = useState<string>("lightBlue");
     
     useEffect(() => {
-        const darkMode = localStorage.getItem("theme") === "dark";
-        setDarkMode(darkMode);
+        const browserDefault = window?.matchMedia('(prefers-color-scheme: dark)')?.matches;
+        const userPrefered = localStorage.getItem("theme") ? localStorage.getItem("theme") === "dark" : undefined;
+
+        setDarkMode(userPrefered || browserDefault || true);
 
         const colorSchema = localStorage.getItem("colorSchema");
         if(PaletteOptions[colorSchema]) setColorSchema(colorSchema);
